6cc0b24ff1675a0a13b98d0d7313426af37fc67c
Snuggs <rashaunstovall@gmail.com>
2017-02-18 17:55:44 -0500

    Add Benchmark & Performance Notes

--- benchmark.js
+++ benchmark.js
@@ -7,14 +7,22 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Node#id
 // https://developer.mozilla.org/en-US/docs/Web/API/Node#Examples
 // WOW!!! elements.children.length=0 faster REALLY?
+//
 //   Also remove lastChild is (marginally) faster as well
+//   https://jsperf.com/empty-an-element/4
 //
 // How is `Element.innerHTML` so gawd dayumn slow?
 //   I get it has to creat a `DomParser` but stil
 //   https://jsperf.com/empty-an-element
 //
 // https://jsperf.com/empty-an-element/2
+// https://jsperf.com/empty-an-element/16
+// But `innerHtml` is faster here? WTF
+// https://jsperf.com/innerhtml-vs-removechild/67
 //
+
+// setting display:none causes no reflow afterwards.
+// http://stackoverflow.com/questions/2447309/javascript-removechild-and-appendchild-vs-display-none-and-display-blockinl
 
 console.group ('appendChild')
 console.time ('append time')
