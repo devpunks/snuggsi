351f25e10bdbfd3e071d9b3601ce93b7d23a22c8
Snuggs <rashaunstovall@gmail.com>
2017-04-01 21:19:23 -0400

    Bump distribution

--- dist/snuggsi.es
+++ dist/snuggsi.es
@@ -1,3 +1,167 @@
+function State ( context, handler = _ => {} ) {
+  this.subscribe = callback => handler = callback
+
+  const
+    history = new Array (context)
+  , clone   = context => JSON.parse
+      (JSON.stringify (context))
+
+  , thunk = property =>
+      [ property,
+        {
+          get: _ => history
+            [history.length-1] [property],
+
+          set (value) {
+            const next  = clone
+              (previous = history [history.length-1])
+
+            next [property] = value
+            handler (previous, next)
+            history [history.length] = next
+          }
+        }
+      ]
+
+  for (property in context)
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+    Object.defineProperty (this, ...thunk (property))
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties
+}
+function tokenize (fragment) {
+  const
+    tokens = []
+
+  , tail = (text, sibling) =>
+      ( ! text.after (sibling)) && sibling
+
+  // https://www.merriam-webster.com/dictionary/sift
+  , sift = text =>
+      text.textContent.match (/({\w+})/) && (tokens [tokens.length] = text)
+      || text
+
+  for (match of mine (fragment))
+    slice (match.textContent)
+      .map (sift)
+      .reduce (tail, match)
+
+    && match.remove ()
+
+  return tokens
+
+  // deconstruct
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/after
+    // Would like to use children
+    // to use Element.insertAdjacentElement ('afterend', text)
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
+    // however, text is a Node not an Element
+    // WARNING: NO DocumentFragment support
+    // https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/children#Browser_compatibility
+    // https://developer.mozilla.org/en-US/docs/Web/API/Node/insertBefore
+    // BENCHMARK: https://jsperf.com/insertadjacenthtml-perf/3
+}
+
+function zip
+  (...elements) { const zipper = []
+
+  , lock = (zipper, row) => [...zipper, ...row]
+  , pair = teeth  => // http://english.stackexchange.com/questions/121601/pair-or-couple
+    // thunk
+      (tooth, position) => [tooth, teeth [position]]
+
+  return elements [1]
+    .map (pair (elements [0]))
+    .reduce (lock)
+}
+
+function slice
+  (text) { const tokens  = []
+
+  , match    = /({\w+})/g
+  , replace  = token => collect (token) && '✂️'
+  , collect  = token => tokens.push (token)
+  , sections = text
+      .replace (match, replace)
+        .split ('✂️')
+
+  return zip (tokens, sections)
+     .filter (element => element)
+        .map (element => new Text (element))
+}
+
+function mine // https://www.merriam-webster.com/dictionary/comb#h2
+// http://jsfiddle.net/zaqtg/10
+// http://stackoverflow.com/questions/2579666/getelementsbytagname-equivalent-for-textnodes#answer-2579869
+// https://www.w3.org/TR/DOM-Level-2-Traversal-Range/traversal.html
+// https://developer.mozilla.org/en-US/docs/Web/API/TreeWalker
+// https://developer.mozilla.org/en-US/docs/Web/API/NodeIterator
+// https://developer.mozilla.org/en-US/docs/Web/API/NodeFilter
+// NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_COMMENT | NodeFilter.SHOW_TEXT
+
+(head) {
+  console.time ()
+
+  const nodes = []
+  const walker = document.createNodeIterator
+      (head, NodeFilter.SHOW_TEXT, visit)
+      // by default breaks on template YAY! 🎉
+
+  while (node = walker.nextNode ()) nodes.push (node)
+  console.timeEnd ()
+  return nodes
+}
+
+const tail = (text, sibling) => ( ! text.after (sibling)) && sibling
+
+function visit (node) {
+  return /({\w+})/g.test (node.data)
+    && NodeFilter.FILTER_ACCEPT // <😕  is this even necessary?
+}
+
+function comb (parent) {
+  if (parent.hasChildNodes())
+    for (let node = parent.firstChild; node; node = node.nextSibling)
+      DOMComb (node)
+}
+function Template ( name = 'snuggsi' ) {
+  return Object.assign (factory (...name), { bind })
+
+  function bind (context) {
+    context = (Array.isArray (context) ? context : [context])
+
+    const
+      tokens   = []
+    , rendered = context
+        .map (context => this.content.cloneNode (true))
+        .map (collect, tokens)
+
+    this.innerHTML = ''
+    for (const frame of rendered) this.content.appendChild (frame)
+
+    return context.map(transfer, tokens) && this
+  }
+
+  function factory (name) { return (
+       document.querySelector ('template[name='+name+']').cloneNode (true)
+    || document.createElement ('template')
+  )}
+
+  function collect (fragment) {
+    const objectify = tokens =>
+      tokens.reduce ( (object, token) =>
+        (object [token.textContent.match (/{(.+)}/) [1]]  = token) && object
+      , {})
+
+    return this.push (objectify (tokenize (fragment))) && fragment
+  }
+
+  function transfer (context, index) {
+    for (const property in context) this [index]
+      [property] && (this [index] [property].textContent = context [property])
+  }
+}
 // Usage
 //
 //  Element `date-calendar`
--- dist/snuggsi.js
+++ dist/snuggsi.js
@@ -1,3 +1,180 @@
+function State ( context, handler ) {
+  var this$1 = this;
+  if ( handler === void 0 ) handler = function (_) {};
+
+  this.subscribe = function (callback) { return handler = callback; }
+
+  var
+    history = new Array (context)
+  , clone   = function (context) { return JSON.parse
+      (JSON.stringify (context)); }
+
+  , thunk = function (property) { return [ property,
+        {
+          get: function (_) { return history
+            [history.length-1] [property]; },
+
+          set: function (value) {
+            var next  = clone
+              (previous = history [history.length-1])
+
+            next [property] = value
+            handler (previous, next)
+            history [history.length] = next
+          }
+        }
+      ]; }
+
+  for (property in context)
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+    { Object.defineProperty.apply (Object, [ this$1 ].concat( thunk (property) )) }
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties
+}
+function tokenize (fragment) {
+  var
+    tokens = []
+
+  , tail = function (text, sibling) { return ( ! text.after (sibling)) && sibling; }
+
+  // https://www.merriam-webster.com/dictionary/sift
+  , sift = function (text) { return text.textContent.match (/({\w+})/) && (tokens [tokens.length] = text)
+      || text; }
+
+  for (var i = 0, list = mine (fragment); i < list.length; i += 1)
+    {
+    match = list[i];
+
+    slice (match.textContent)
+      .map (sift)
+      .reduce (tail, match)
+
+    && match.remove ()
+  }
+
+  return tokens
+
+  // deconstruct
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/after
+    // Would like to use children
+    // to use Element.insertAdjacentElement ('afterend', text)
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
+    // however, text is a Node not an Element
+    // WARNING: NO DocumentFragment support
+    // https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/children#Browser_compatibility
+    // https://developer.mozilla.org/en-US/docs/Web/API/Node/insertBefore
+    // BENCHMARK: https://jsperf.com/insertadjacenthtml-perf/3
+}
+
+function zip
+  () {
+  var elements = [], len = arguments.length;
+  while ( len-- ) elements[ len ] = arguments[ len ];
+ var zipper = []
+
+  , lock = function (zipper, row) { return zipper.concat( row); }
+  , pair = function (teeth)  { return function (tooth, position) { return [tooth, teeth [position]]; }; }
+
+  return elements [1]
+    .map (pair (elements [0]))
+    .reduce (lock)
+}
+
+function slice
+  (text) { var tokens  = []
+
+  , match    = /({\w+})/g
+  , replace  = function (token) { return collect (token) && '✂️'; }
+  , collect  = function (token) { return tokens.push (token); }
+  , sections = text
+      .replace (match, replace)
+        .split ('✂️')
+
+  return zip (tokens, sections)
+     .filter (function (element) { return element; })
+        .map (function (element) { return new Text (element); })
+}
+
+function mine // https://www.merriam-webster.com/dictionary/comb#h2
+// http://jsfiddle.net/zaqtg/10
+// http://stackoverflow.com/questions/2579666/getelementsbytagname-equivalent-for-textnodes#answer-2579869
+// https://www.w3.org/TR/DOM-Level-2-Traversal-Range/traversal.html
+// https://developer.mozilla.org/en-US/docs/Web/API/TreeWalker
+// https://developer.mozilla.org/en-US/docs/Web/API/NodeIterator
+// https://developer.mozilla.org/en-US/docs/Web/API/NodeFilter
+// NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_COMMENT | NodeFilter.SHOW_TEXT
+
+(head) {
+  console.time ()
+
+  var nodes = []
+  var walker = document.createNodeIterator
+      (head, NodeFilter.SHOW_TEXT, visit)
+      // by default breaks on template YAY! 🎉
+
+  while (node = walker.nextNode ()) { nodes.push (node) }
+  console.timeEnd ()
+  return nodes
+}
+
+var tail = function (text, sibling) { return ( ! text.after (sibling)) && sibling; }
+
+function visit (node) {
+  return /({\w+})/g.test (node.data)
+    && NodeFilter.FILTER_ACCEPT // <😕  is this even necessary?
+}
+
+function comb (parent) {
+  if (parent.hasChildNodes())
+    { for (var node = parent.firstChild; node; node = node.nextSibling)
+      { DOMComb (node) } }
+}
+function Template ( name ) {
+  if ( name === void 0 ) name = 'snuggsi';
+
+  return Object.assign (factory.apply (void 0, name), { bind: bind })
+
+  function bind (context) {
+    var this$1 = this;
+
+    context = (Array.isArray (context) ? context : [context])
+
+    var
+      tokens   = []
+    , rendered = context
+        .map (function (context) { return this$1.content.cloneNode (true); })
+        .map (collect, tokens)
+
+    this.innerHTML = ''
+    for (var i = 0, list = rendered; i < list.length; i += 1) {
+      var frame = list[i];
+
+      this$1.content.appendChild (frame)
+    }
+
+    return context.map(transfer, tokens) && this
+  }
+
+  function factory (name) { return (
+       document.querySelector ('template[name='+name+']').cloneNode (true)
+    || document.createElement ('template')
+  )}
+
+  function collect (fragment) {
+    var objectify = function (tokens) { return tokens.reduce ( function (object, token) { return (object [token.textContent.match (/{(.+)}/) [1]]  = token) && object; }
+      , {}); }
+
+    return this.push (objectify (tokenize (fragment))) && fragment
+  }
+
+  function transfer (context, index) {
+    var this$1 = this;
+
+    for (var property in context) { this$1 [index]
+      [property] && (this$1 [index] [property].textContent = context [property]) }
+  }
+}
 // Usage
 //
 //  Element `date-calendar`
