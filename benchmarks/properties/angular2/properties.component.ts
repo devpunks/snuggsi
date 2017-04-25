ff240e7562f45a549376a0b67ea180c844d15305
Snuggs <rashaunstovall@gmail.com>
2017-04-24 20:49:15 -0400

    More implementing Angular 2 Benchmark

---
+++ benchmarks/properties/angular2/properties.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/properties.component.html'
+})
+export class AppComponent {
+    constructor() {}
+
+      name: string = 'Angular2';
+}
