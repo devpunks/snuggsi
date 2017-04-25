fb36e231b85512731c697ceabc4b4a0feff01feb
Snuggs <rashaunstovall@gmail.com>
2017-04-24 23:51:22 -0400

    Update Angular Benchmarking

--- benchmarks/properties/angular2/properties.component.ts
+++ benchmarks/properties/angular2/properties.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+// http://www.edc4it.com/blog/web/helloworld-angular2.html
+// http://www.thejavageek.com/2017/03/28/hello-world-app-angular-4/
+//our root app component
+import {Component, NgModule, VERSION} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
 @Component({
-    selector: 'my-app',
-    templateUrl: 'app/properties.component.html'
+  selector: 'my-app',
+  template: `
+    <div>
+      <h2>Hello {{name}}</h2>
+    </div>
+  `,
 })
-export class AppComponent {
-    constructor() {}
-
-      name: string = 'Angular2';
+export class App {
+  name:string;
+  constructor() {
+    this.name = `Angular! v${VERSION.full}`
+  }
 }
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ App ],
+  bootstrap: [ App ]
+})
+export class AppModule {}
