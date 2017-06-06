5ab5c2e7bd3a85a26308eb009a3167caa7b4af8c
Snuggs <rashaunstovall@gmail.com>
2017-06-06 02:31:14 -0400

    :muscle: bump distribution for dist/snuggsi.js

--- dist/snuggsi.js
+++ dist/snuggsi.js
@@ -256,47 +256,43 @@ var GlobalEventHandlers = function (Element) { return ((function (Element) {
 
   anonymous.prototype.register = function (onevents) {
     var this$1 = this;
-    if ( onevents === void 0 ) onevents = function (attr) { return /^on/.test (attr.name || attr); };
+    if ( onevents === void 0 ) onevents = function (attr) { return /^on/.test (attr); };
 
 
     var
-      mirror = function (handler) { return (this$1 [handler] === null) && // ensure W3C on event
-          (this$1 [handler] = Element [handler].bind (this$1)); }
+      mirror = function (handler) { return onevents (handler) &&
+        (this$1 [handler] === null) && // ensure W3C on event
+        (this$1 [handler] = Element [handler].bind (this$1)); }
 
     , nodes = // CSS :not negation https://developer.mozilla.org/en-US/docs/Web/CSS/:not
         // How can we select elements with on* attribute? (i.e. <... onclick=foo onblur=bar>)
         // If we can do this we can only retrieve the elements that have a traditional inline event.
         // This is theoretically more performant as most elements won't need traditional event registration.
-        ':not(script):not(template):not(style):not(link)' // remove metadata elements
+
+//      ':not(script):not(template):not(style):not(link)' // remove metadata elements
+
+    '*'
 
     , children =
         Array.from (this.querySelectorAll (nodes))
 
     , reflect = function (node) { return Array
           .from (node.attributes)
+          .map  (function (attr) { return attr.name; })
           .filter (onevents)
           .map (reflection (node)); }
 
     , reflection =
-        function (node) { return function (event) { return (node [event] = handle (node [event])); }; }
-
-    , handle =
-        function (handler, ref) {
-            if ( ref === void 0 ) ref = (/{\s*(\w+)\s*}/.exec (handler) || []);
-            var _ = ref[0];
-            var event = ref[1];
-
-            return event
-            && Element [event]
-            && Element [event].bind (this$1)
-            || handler // existing handler
-            || null;
-    }  // default for W3C on* event handlers
+        function (node) { return function (event) { return node [event] =
+              /{\s*(\w+)\s*}/.exec (node [event])
+              && Element [event]
+              && Element [event].bind (this$1)
+              || node [event] // existing handler
+              || null; }; }  // default for W3C on* event handlers
 
 
     Object // mirror instance events to element
       .getOwnPropertyNames (Element)
-      .filter (onevents)
       .map (mirror)
 
     void [this] // reflect events from Element
