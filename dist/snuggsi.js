15b0cf49fde83f61118af98e0e6e2fa2d89031cf
Snuggs <rashaunstovall@gmail.com>
2017-09-14 09:49:38 -0400

    :muscle: bump TRANSPILED distribution dist/snuggsi.js

--- dist/snuggsi.js
+++ dist/snuggsi.js
@@ -191,27 +191,6 @@ new (function () {
 
   return CustomElementRegistry;
 }())
-var HTMLLinkElement = function
-
-  // http://w3c.github.io/webcomponents/spec/imports/#h-interface-import
-
-(tag) {
-
-  var
-    evt = new Event ('load')
-
-  , target = document.querySelector // use CSS :any ?
-      ('link[href*='+tag+'][rel=import]')
-
-  // https://github.com/webcomponents/html-imports#htmlimports
-  ;(evt.target = target)
-    && window.HTMLImports
-    && !!! window.HTMLImports.useNative
-    && HTMLImports.whenReady ( function (_) { return target.dispatchEvent (evt); } ) // eww
-
-  return target
-}
-
 var TokenList = function (node) {
 
   this
