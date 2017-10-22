9391c1f58e8a80db14e24f5387a5842506eb566a
Snuggs <rashaunstovall@gmail.com>
2017-10-22 00:57:39 -0400

    Remove comments from snuggsi.es

--- dist/snuggsi.js
+++ dist/snuggsi.js
@@ -90,27 +90,6 @@ DOMTokenList.prototype.bind = function (context) {
     .map(restore)
 };
 
-// Preloading - https://w3c.github.io/preload
-// $$$$ loading capabilities - https://pie.gd/test/script-link-events/
-// $$$$ when is a stylesheet really loaded? https://www.phpied.com/when-is-a-stylesheet-really-loaded/
-// IE11 Support for Prerender / Prefetch - https://msdn.microsoft.com/en-us/library/dn265039(v=vs.85).aspx
-// Resource Hints - https://www.w3.org/TR/resource-hints
-// https://jakearchibald.com/2017/h2-push-tougher-than-i-thought/#push-vs-preload
-// http://w3c.github.io/webcomponents/spec/imports/#h-interface-import
-// https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/12142852/
-// Caching best practices - https://jakearchibald.com/2016/caching-best-practices/
-// $$$$$$ HOT SWAP URL BASE PATHS! https://eager.io/blog/three-real-world-use-cases-for-mutation-observer
-//
-//
-// Link in body
-//
-// https://github.com/whatwg/html/commit/179983e9eb99efe417349a40ebb664bd11668ddd
-// https://bugs.webkit.org/show_bug.cgi?id=172639
-// https://github.com/whatwg/html/pull/616#issuecomment-180018260
-// future links
-
-// Prefetch implementation
-//
 void (function (_) {
   // https://bugs.webkit.org/show_bug.cgi?id=38995
   // https://www.w3.org/TR/html5/document-metadata.html#the-link-element
