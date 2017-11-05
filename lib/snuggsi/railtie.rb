a0d114164789d2bf29c7e5fe418cba7b6d954d79
Snuggs <rashaunstovall@gmail.com>
2017-11-05 15:33:46 +0100

    Add Snuggsi module

--- lib/snuggsi/railtie.rb
+++ lib/snuggsi/railtie.rb
@@ -1,16 +1,18 @@
 require 'action_view'
 require 'sprockets/railtie'
 
-class WebComponentsRails::Railtie < Rails::Railtie
+class Snuggsi::Railtie < Rails::Railtie
   # Register our asset tag helpers
   initializer 'web_components.asset_tag_helper' do
+
     ActionView::Base.module_eval do
-      include WebComponentsRails::AssetTagHelper
+      include Snuggsi::AssetTagHelper
     end
+
     # Certain run modes in Rails (like rails g migration), won't have assets loaded
     if Rails.application.assets.present?
       Rails.application.assets.context_class.class_eval do
-        include WebComponentsRails::AssetTagHelper
+        include Snuggsi::AssetTagHelper
       end
     end
   end
@@ -20,9 +22,9 @@ class WebComponentsRails::Railtie < Rails::Railtie
     app.config.assets.configure do |env|
       env.register_mime_type 'text/html', extensions: ['.html', '.haml']
       env.register_preprocessor 'text/html', Sprockets::DirectiveProcessor
-      env.register_preprocessor 'text/html', WebComponentsRails::HTMLImportProcessor
+      env.register_preprocessor 'text/html', Snuggsi::HTMLImportProcessor
       env.register_bundle_processor 'text/html', Sprockets::Bundle
-      env.register_bundle_processor 'text/html', WebComponentsRails::HTMLBundleProcessor
+      env.register_bundle_processor 'text/html', Snuggsi::HTMLBundleProcessor
     end
   end
 end
