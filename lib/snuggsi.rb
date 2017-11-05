d1412b1ac52853cec622231a11a2bed6b4d501fc
Snuggs <rashaunstovall@gmail.com>
2017-11-05 15:57:23 +0100

    Add require libraries

--- lib/snuggsi.rb
+++ lib/snuggsi.rb
@@ -2,3 +2,13 @@ module Snuggsi
   # Your code goes here...
   mattr_accessor :optimize_scripts
 end
+
+require 'sprockets'
+require 'nokogumbo'
+require 'active_support'
+
+require 'snuggsi/asset_tag_helper'
+require 'snuggsi/html_bundle_processor'
+require 'snuggsi/html_import_processor'
+require 'snuggsi/railtie'
+require 'snuggsi/version'
