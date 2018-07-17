2d7d0086aa6e2945d0d5673cc0ab5b5fb1d22341
Snuggs <rashaunstovall@gmail.com>
2018-07-17 00:02:03 -0400

    :muscle: bump TRANSPILED distribution dist/snuggsi.js

--- dist/snuggsi.js
+++ dist/snuggsi.js
@@ -493,7 +493,6 @@ var EventTarget = function (HTMLElement) { return ((function (HTMLElement) {
 
   anonymous.prototype.renderable = function ( handler ) {
     var this$1 = this;
-    if ( handler === void 0 ) handler = function () {};
 
 
     // BIG BUG IN IE!!!
